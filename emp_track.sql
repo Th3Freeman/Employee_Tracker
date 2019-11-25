7aed223111ac3a3b758f71a777ef564fcb6a6b18
Th3freeman <thefrman345@gmail.com>
2019-11-25 03:13:21 -0500

    built initial prompt for user and a seed for my db to test with

--- emp_track.sql
+++ emp_track.sql
@@ -9,23 +9,29 @@ CREATE TABLE _department(
 );
 
 CREATE TABLE _role(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(9,2) NOT NULL,
-    department_id INT(3) ZEROFILL NOT NULL,
+    department_id INT(11),
     PRIMARY KEY (id)
     );
 
 CREATE TABLE _employee(
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT(11) NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT(3) ZEROFILL NOT NULL,
-    manager_id INT(3) ZEROFILL,
+    role_id INT(11),
     PRIMARY KEY (id)
     );
     
-    
+INSERT INTO _employee (first_name, last_name, role_id)
+VALUES ("Mark" , "Jacobs", 1), ("Jeffery", "Martinson", 2), ("Agnus", "Purdue", 3);
+
+INSERT INTO _role (title, salary, department_id)
+VALUES ("Janitor", 20000, 1), ("CEO",5000000, 2), ("Secretary", 50000, 3);
+
+INSERT INTO _department (_name)
+VALUES ("Maintenance"), ("Management"), ("Customer Service");
     
     
 
--- emp_tracker.js
+++ emp_tracker.js
@@ -10,3 +10,54 @@ var connection = mysql.createConnection({
   password: "rootroot",
   database: "Employee_Tracker_DB"
 });
+
+// connects to server and begins prompting the user
+connection.connect(function (err) {
+  if (err) throw err;
+
+  begin();
+});
+
+function begin() {
+  inquirer
+    .prompt({
+      name: "initialChoices",
+      type: "list",
+      message: "Welcome, What would you like to do?",
+      choices: ["View Employees", "Add Employee", "Remove Employee", "Update Employee Role", "View Department", "Add Department", "Remove Department", "View All Titles"]
+    })
+    .then(function (answer) {
+      if (answer.initialChoices === "View Employees") {
+        // viewEmps();
+      }
+      else if (answer.initialChoices === "Add Employee") {
+        addEmp();
+      }
+      else if (answer.initialChoices === "Remove Employee") {
+        remEmp();
+      }
+      else if (answer.initialChoices === "Update Employee Role") {
+        updEmpRole();
+      }
+      else if (answer.initialChoices === "View Department") {
+        viewDept();
+      }
+      else if (answer.initialChoices === "Add Department") {
+        addDept();
+      }
+      else if (answer.initialChoices === "Remove Department") {
+        remDept();
+      }
+      else if (answer.initialChoices === "View All Titles") {
+        viewRoles();
+      }
+      else {
+        connection.end();
+      }
+    });
+}
+
+console.log(console.table);
+
+
+
